75da75f1db905a7428251c84a6c70aed1de0e735
David Contreras <david.b.contreras@gmail.com>
2023-09-17 14:35:47 -0400

    lambda refresher

--- app/src/main/java/com/davecon/kotlinlab/main.kt
+++ app/src/main/java/com/davecon/kotlinlab/main.kt
@@ -1,7 +1,7 @@
 package com.davecon.kotlinlab
 
 fun main() {
-
+    //region Lambdas
     /**
      * This is an example of a function
      * variable: none
@@ -27,4 +27,28 @@ fun main() {
 
     val catAge: (Int) -> Int = { humanAge -> humanAge * 7 }
     println("Cat age lambda: " + catAge(3))
+
+    /**
+     * This lambda returns a Unit which is the equivalent to void in Java
+     * For this, we remove the type reference and just use "it" for a scope reference
+     */
+    val name: (String) -> Unit = { println("Name lambda: $it") }
+    name("Dave")
+
+    /**
+     * If the LAST parameter of a function IS a function, we can use a trailing lambda
+     * This allows for a cleaner syntax. We see this when we invoke enhancedMessage
+     */
+    fun enhancedMessage(message: String, funAsParameter: () -> Int) {
+        println("Trailing Lambda: $message ${funAsParameter()}")
+    }
+    enhancedMessage("funAsParameter") { addLambda(1, 2) }
+
+    //region Collections: List Methods, Sets, Maps, Empty, Filters
+
+
+    //region OOP Concepts: Primary Constructors, Inits, Inheritance, Interface, Extensions, Data Class
+
+
+    //region Advanced Concepts: Generics, Enums, State, Sealed Classes,
 }
