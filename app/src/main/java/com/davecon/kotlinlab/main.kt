5faa3c0685c570fbeffbaa97e82778d73c5d991e
David Contreras <david.b.contreras@gmail.com>
2023-09-17 15:29:53 -0400

    collections refresher with filter example

--- app/src/main/java/com/davecon/kotlinlab/main.kt
+++ app/src/main/java/com/davecon/kotlinlab/main.kt
@@ -45,7 +45,48 @@ fun main() {
     enhancedMessage("funAsParameter") { addLambda(1, 2) }
 
     //region Collections: List Methods, Sets, Maps, Empty, Filters
-
+    /**
+     * Collections are immutable by default, they are the term given for:
+     * lists, sets, and maps
+     * Methods:
+     * .add() - add element
+     * .addAll() - add all elements
+     * .remove() - remove element
+     * .removeAt() - remove element at index
+     * .removeAll() - remove all elements
+     * .clear() - remove all elements
+     * .contains() - return true if collection contains element
+     * .size() - return size of collection
+     * .indexOf() - return index of element
+     * .replace() - replace element at index
+     * .replaceAll() - replace all elements
+     * .put() - add element to a map
+     * .values() - return a collection of values from a map
+     * "mutable" keyword to make them mutable
+     * "listOf" keyword to create a list
+     * "setOf" keyword to create a set
+     * "mapOf" keyword to create a map
+     * "emptyList" keyword to create an empty list
+     * "emptySet" keyword to create an empty set
+     * "emptyMap" keyword to create an empty map
+     * "filter" keyword to filter a list
+     * "filterNot" keyword to filter a list
+     * "filterNotNull" keyword to filter a list
+     * "filterIndexed" keyword to filter a list
+     * "filterIndexedTo" keyword to filter a list
+     * "filterIsInstance" keyword to filter a list
+     * "filterNotTo" keyword to filter a list
+     * "filterNotNullTo" keyword to filter a list
+     * "filterTo" keyword to filter a list by a predicate
+     *
+     * Instantiate collections without data:
+     * val emptyList = emptyList<Type>()
+     * val emptySet = emptySet<Type>()
+     * val emptyMap = emptyMap<Type, Type>()
+     */
+    val listOfNames = listOf("Dave", "John", "Jane", "Rachel", "Rafael")
+    val startsWithREndsWithL = listOfNames.filter { it.startsWith("R") && it.endsWith("l") }
+    println(startsWithREndsWithL)
 
     //region OOP Concepts: Primary Constructors, Inits, Inheritance, Interface, Extensions, Data Class
 
